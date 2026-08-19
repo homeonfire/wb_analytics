48d9b8b6f9fb68f0a48622c0bcf1bb79622eca35
homeonfire <getintouch.hof@gmail.com>
2026-08-19 16:08:10 +0300

    Trust Docker gateway proxy headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '127.0.0.1');
+        $middleware->trustProxies(at: '172.17.0.1');
 
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
